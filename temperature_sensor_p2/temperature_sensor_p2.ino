408d6baa221be581334b4edcf5c2a8a5e7a50639
Nate <lao.nathan@yahoo.com>
2022-09-22 23:12:40 -0500

    add LCD support

--- temperature_sensor_p2/temperature_sensor_p2.ino
+++ temperature_sensor_p2/temperature_sensor_p2.ino
@@ -1,6 +1,11 @@
-// https://electronoobs.com/eng_arduino_tut12.php
+
+
+#define LCD_ENABLE 1
 
 #include <DHT.h>
+#if LCD_ENABLE
+#include <LiquidCrystal.h>
+#endif
 
 #include "isr_timer.hpp"
 #include "temp.hpp"
@@ -18,6 +23,20 @@ const TEMP::DATA_BUFFER sensor_offset = {-1.66};
 TEMP::DATA_BUFFER sensor_buffer;
 TEMP::Sensor *sensor = 0;
 
+#if LCD_ENABLE
+// LCD Pins (if available)
+const int pin_RS = 8;
+const int pin_EN = 9;
+const int pin_d4 = 4;
+const int pin_d5 = 5;
+const int pin_d6 = 6;
+const int pin_d7 = 7;
+const int pin_BL = 10;
+
+// LCD Object
+LiquidCrystal *lcd = 0;
+#endif
+
 void setup()
 {
     // Initialize Interrupt Variables
@@ -28,22 +47,38 @@ void setup()
     // Setup 1 Hz Timer Interrupt
     cli(); // Disable Interupts
     TIMER::setISRTimer(1);
-    sei();  // Enable Interrupts
+    sei(); // Enable Interrupts
 
     // Initialize Temperature Sensor and Buffer
     memset(&sensor_buffer, 0, sizeof(sensor_buffer));
     sensor = new TEMP::Sensor(DHTPIN, DHTTYPE, &sensor_buffer, &sensor_offset);
 
-    // Setup Serial and Start CSV Loggin
+    // Setup Serial and Start CSV Logging
     Serial.begin(9600);
     Serial.println("time_sec,temperature_f");
+
+#if LCD_ENABLE
+    lcd = new LiquidCrystal(pin_RS, pin_EN, pin_d4, pin_d5, pin_d6, pin_d7);
+    lcd->begin(16,2); // 16 col, 2 rows
+    lcd->setCursor(0,0);
+    lcd->print("TIME");
+    lcd->setCursor(8,0);
+    lcd->print("TEMP (F)");
+#endif
 }
 
 void loop()
 {
     // Sample on every frame and update local buffer
     sensor->sample();
- 
+
+#if LCD_ENABLE
+    lcd->setCursor(0,1);
+    lcd->print(time);
+    lcd->setCursor(8,1);
+    lcd->print(sample.temp_f);
+#endif
+
     // If the interval trigger is set - log out to Serial
     // Note: this operation is SLOW - interrupts SHOULD BE enabled
     if (trigger)
