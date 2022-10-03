2eac65e69c6fc0e9916f3a5c7a575d1b32fcb350
Nate <lao.nathan@yahoo.com>
2022-10-02 21:19:41 -0500

    add serial output

--- p3_tachometer/p3_tachometer.ino
+++ p3_tachometer/p3_tachometer.ino
@@ -14,18 +14,20 @@ const int pin_d5 = 5;
 const int pin_d6 = 6;
 const int pin_d7 = 7;
 const int pin_BL = 10;
-const unsigned long LCD_DELAY = 8000; // Delay time (1 second = 8000 frames for 8kHz Interrupt)
-volatile unsigned long lcd_time;      // Tracks the update LCD frame
-volatile bool lcd_flag;               // Indicates LCD update
+const unsigned long LCD_DELAY = 1000;   // LCD Delay Time in milliseconds
+const unsigned long DELAY_FACTOR = 8;   // Do not modify, LCD_DELAY multiplier
+volatile unsigned long lcd_time;        // Tracks the update LCD frame
+volatile bool lcd_flag;                 // Indicates LCD update
 LiquidCrystal *lcd = 0;
 
 // Motor Variables
-const uint8_t PIN_MOTOR = 3;          // Motor Pin
-const unsigned int RAMP_DELAY = 200;  // Ramp Speed delay in Milliseconds
-const unsigned char MIN_SPEED = 50;   // Minimal Speed (duty cycle)
-const unsigned char MAX_SPEED = 0xFF; // Maximum Speed (duty cycle)
-unsigned char speed;                  // Current Speed
-bool ramp_up;                         // Ramp Up flag
+const uint8_t PIN_MOTOR = 3;            // Motor Pin
+const unsigned int RAMP_DELAY = 200;    // Ramp Speed delay in Milliseconds
+const unsigned char MIN_SPEED = 50;     // Minimal Speed (duty cycle)
+const unsigned char MAX_SPEED = 0xFF;   // Maximum Speed (duty cycle)
+unsigned char speed;                    // Current Speed
+bool ramp_up;                           // Ramp Up flag
+unsigned int time;                  // Current Time in Milliseconds
 
 // Tachometer Variables
 const uint8_t PIN_IR = A1;                            // IR Phototransitor Pin
@@ -51,6 +53,10 @@ void setup()
     // Setup Motor PWM Pin
     pinMode(PIN_MOTOR, OUTPUT);
 
+    // Setup Serial
+    Serial.begin(9600);
+    Serial.println("time,speed,rpm");
+
     // Setup LCD
     lcd = new LiquidCrystal(pin_RS, pin_EN, pin_d4, pin_d5, pin_d6, pin_d7);
     lcd->begin(16, 2); // 16 col, 2 rows
@@ -59,9 +65,14 @@ void setup()
     lcd->setCursor(8, 0);
     lcd->print("RPM");
 
+    // Setup LCD variables
+    lcd_time = 0;
+    lcd_flag = false;
+
     // Setup Motor to Ramp Up
     ramp_up = true;
     speed = MIN_SPEED;
+    time = 0;
 
     // Setup tachometer variables
     frames = 0;
@@ -69,9 +80,7 @@ void setup()
     current_phase = 0;
     rpm = 0;
 
-    // Setup LCD flags
-    lcd_time = 0;
-    lcd_flag = false;
+
 }
 
 ISR(TIMER1_COMPA_vect)
@@ -98,20 +107,17 @@ ISR(TIMER1_COMPA_vect)
         digitalWrite(PIN_DEBUG, LOW);
     }
 
-    if (lcd_time == LCD_DELAY)
+    // Issue LCD update
+    if (lcd_time == LCD_DELAY * DELAY_FACTOR)
     {
         lcd_flag = true;
         lcd_time = 0;
     }
-
     lcd_time++;
 }
 
 void loop()
 {
-    // Ramp Up and Ramp Down the Motor
-    delay(RAMP_DELAY);
-
     // Determine ramp phase if at limits
     if (speed == MIN_SPEED)
         ramp_up = true;
@@ -127,18 +133,25 @@ void loop()
     // Set duty cycle to control speed
     analogWrite(PIN_MOTOR, speed);
 
+    // Poll the measured phase delta
+    cli();
+    current_phase = measured_phase;
+    sei();
+
+    // Calculate RPM
+    current_phase = (current_phase * PROP_BLADES) * FRAME_USEC;
+    rpm = RPM_FACTOR_USEC / current_phase;
+
+    // Log out to host
+    Serial.print(time);
+    Serial.print(",");
+    Serial.print(speed);
+    Serial.print(",");
+    Serial.println(rpm);
+
     // Display RPM at 1 second intervals
     if (lcd_flag)
     {
-        // Poll the measured phase delta
-        cli();
-        current_phase = measured_phase;
-        sei();
-
-        // Calculate RPM
-        current_phase = (current_phase * PROP_BLADES) * FRAME_USEC;
-        rpm = RPM_FACTOR_USEC / current_phase;
-
         // Display LCD
         lcd->setCursor(0, 1);
         lcd->print(speed);
@@ -148,4 +161,8 @@ void loop()
         lcd->print("    ");
         lcd_flag = false;
     }
+
+    // Ramp Delay
+    delay(RAMP_DELAY);
+    time += RAMP_DELAY;
 }
