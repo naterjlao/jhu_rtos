ea3d3340715067339aa0a1a27d778109b4ccc2ba
flypi <flypi@pi.com>
2022-11-18 03:33:41 +0000

    breadcrumbs rpi helloworld

---
+++ p8_drone_imu/rpi/flypi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+//https://stackoverflow.com/questions/49577244/serial-communication-between-rpi-and-arduino-using-c
+
+int main()
+{
+    cout << "test" << endl;
+    return 0;
+}
