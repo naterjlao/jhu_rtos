69644e3dcf451be9c067af6eefff8553dc831df0
Nathaniel Ryan James Lao <lao.nathan@yahoo.com>
2022-11-26 16:28:54 -0600

    minor lines

--- px_drone/host/attitude_indicator/attitude_indicator_gui.py
+++ px_drone/host/attitude_indicator/attitude_indicator_gui.py
@@ -4,18 +4,10 @@ import numpy as np
 
 INST_DIMENSION = 512
 
-
 INST_SKY_COLOR = (238,176,8)
 INST_GROUND_COLOR = (26,45,54)
 INST_MARKER_COLOR = (255,255,255)
 INST_INDICATOR_COLOR = (30,100,255)
-
-
-'''
-cv2.rectangle(output,(128,128),(384,384),(0,255,0),3)
-M = cv2.getRotationMatrix2D((INST_DIMENSION/2, INST_DIMENSION/2),-30,1)
-output = cv2.warpAffine(output,M,(INST_DIMENSION,INST_DIMENSION))
-'''
 
 def attitude_indicator_instrument(pitch: float, roll: float):
     output = np.zeros((INST_DIMENSION,INST_DIMENSION,3), np.uint8)
@@ -36,12 +28,26 @@ cv2.circle(__outer_circle_mask,\
         (INST_DIMENSION // 2, INST_DIMENSION // 2), \
         7 * (INST_DIMENSION // 18), 0, -1) # Diameter is 7/9 -> convert to radians
 
-pitch_displacement = lambda pitch: int(INST_DIMENSION * (pitch / 90.0))
+# Displayed pitch is directly proportional to the raw pitch, whereas 90 degrees is maximum
+__pitch_displacement = lambda pitch: int(INST_DIMENSION * (pitch / 90.0))
+
+# Prerendered Roll Indicators
+__roll_x_coor = lambda radius, roll: (INST_DIMENSION // 2) + int((radius) * np.sin(roll * np.pi / 180.0))
+__roll_y_coor = lambda radius, roll: (INST_DIMENSION // 2) - int((radius) * np.cos(roll * np.pi / 180.0))
+__0_roll_displacement = (INST_DIMENSION // 2, 0)
+__r10_roll_displacement = (__roll_x_coor((INST_DIMENSION / 2) - 20, 10.0),__roll_y_coor((INST_DIMENSION / 2) - 20, 10.0))
+__r20_roll_displacement = (__roll_x_coor((INST_DIMENSION / 2) - 20, 20.0),__roll_y_coor((INST_DIMENSION / 2) - 20, 20.0))
+__r30_roll_displacement = (__roll_x_coor((INST_DIMENSION / 2) - 10, 30.0),__roll_y_coor((INST_DIMENSION / 2) - 10, 30.0))
+__r60_roll_displacement = (__roll_x_coor((INST_DIMENSION / 2) - 10, 60.0),__roll_y_coor((INST_DIMENSION / 2) - 10, 60.0))
+__l10_roll_displacement = (__roll_x_coor((INST_DIMENSION / 2) - 20, -10.0),__roll_y_coor((INST_DIMENSION / 2) - 20, -10.0))
+__l20_roll_displacement = (__roll_x_coor((INST_DIMENSION / 2) - 20, -20.0),__roll_y_coor((INST_DIMENSION / 2) - 20, -20.0))
+__l30_roll_displacement = (__roll_x_coor((INST_DIMENSION / 2) - 10, -30.0),__roll_y_coor((INST_DIMENSION / 2) - 10, -30.0))
+__l60_roll_displacement = (__roll_x_coor((INST_DIMENSION / 2) - 10, -60.0),__roll_y_coor((INST_DIMENSION / 2) - 10, -60.0))
+
 def __render_pitch(src: np.ndarray, pitch: float) -> np.ndarray:
     output = src
 
-    # Displayed pitch is directly proportional to the raw pitch, whereas 90 degrees is maximum
-    pitch_pos = INST_DIMENSION // 2 + pitch_displacement(pitch)
+    pitch_pos = INST_DIMENSION // 2 + __pitch_displacement(pitch)    
 
     # Outer circle
     outer = np.zeros((INST_DIMENSION,INST_DIMENSION,3), np.uint8)
@@ -49,6 +55,15 @@ def __render_pitch(src: np.ndarray, pitch: float) -> np.ndarray:
     cv2.rectangle(outer,(0,INST_DIMENSION // 2),(INST_DIMENSION,INST_DIMENSION),INST_GROUND_COLOR,-1)
     cv2.circle(outer,(INST_DIMENSION // 2, INST_DIMENSION // 2), 7 * (INST_DIMENSION // 18) + 1, (128,128,128), -1)
     cv2.line(outer, (0,INST_DIMENSION//2), (INST_DIMENSION,INST_DIMENSION //2),INST_MARKER_COLOR,6)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __0_roll_displacement,INST_MARKER_COLOR,5)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __r10_roll_displacement,INST_MARKER_COLOR,3)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __r20_roll_displacement,INST_MARKER_COLOR,3)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __r30_roll_displacement,INST_MARKER_COLOR,5)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __r60_roll_displacement,INST_MARKER_COLOR,5)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __l10_roll_displacement,INST_MARKER_COLOR,3)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __l20_roll_displacement,INST_MARKER_COLOR,3)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __l30_roll_displacement,INST_MARKER_COLOR,5)
+    cv2.line(outer, (INST_DIMENSION//2, INST_DIMENSION//2), __l60_roll_displacement,INST_MARKER_COLOR,5)
     outer = cv2.bitwise_and(outer, outer, mask=__outer_circle_mask)
 
     # Inner circle
@@ -56,10 +71,10 @@ def __render_pitch(src: np.ndarray, pitch: float) -> np.ndarray:
     cv2.rectangle(inner,(0,0),(INST_DIMENSION,INST_DIMENSION),INST_SKY_COLOR,-1)
     cv2.rectangle(inner,(0,pitch_pos),(INST_DIMENSION,INST_DIMENSION),INST_GROUND_COLOR,-1)
     cv2.line(inner, (0,pitch_pos), (INST_DIMENSION,pitch_pos),INST_MARKER_COLOR,2)
-    cv2.line(inner, (INST_DIMENSION // 2 - 60,pitch_pos + pitch_displacement(-20.0)), (INST_DIMENSION // 2 + 60,pitch_pos + pitch_displacement(-20.0)),INST_MARKER_COLOR,2)
-    cv2.line(inner, (INST_DIMENSION // 2 - 30,pitch_pos + pitch_displacement(-10.0)), (INST_DIMENSION // 2 + 30,pitch_pos + pitch_displacement(-10.0)),INST_MARKER_COLOR,2)
-    cv2.line(inner, (INST_DIMENSION // 2 - 30,pitch_pos + pitch_displacement(10.0)),  (INST_DIMENSION // 2 + 30,pitch_pos + pitch_displacement(10.0)),INST_MARKER_COLOR,2)
-    cv2.line(inner, (INST_DIMENSION // 2 - 60,pitch_pos + pitch_displacement(20.0)),  (INST_DIMENSION // 2 + 60,pitch_pos + pitch_displacement(20.0)),INST_MARKER_COLOR,2)
+    cv2.line(inner, (INST_DIMENSION // 2 - 60,pitch_pos + __pitch_displacement(-20.0)), (INST_DIMENSION // 2 + 60,pitch_pos + __pitch_displacement(-20.0)),INST_MARKER_COLOR,2)
+    cv2.line(inner, (INST_DIMENSION // 2 - 30,pitch_pos + __pitch_displacement(-10.0)), (INST_DIMENSION // 2 + 30,pitch_pos + __pitch_displacement(-10.0)),INST_MARKER_COLOR,2)
+    cv2.line(inner, (INST_DIMENSION // 2 - 30,pitch_pos + __pitch_displacement(10.0)),  (INST_DIMENSION // 2 + 30,pitch_pos + __pitch_displacement(10.0)),INST_MARKER_COLOR,2)
+    cv2.line(inner, (INST_DIMENSION // 2 - 60,pitch_pos + __pitch_displacement(20.0)),  (INST_DIMENSION // 2 + 60,pitch_pos + __pitch_displacement(20.0)),INST_MARKER_COLOR,2)
     inner = cv2.bitwise_and(inner, inner, mask=__inner_circle_mask)
 
     output = cv2.add(output, outer)
@@ -105,7 +120,7 @@ if __name__ == "__main__":
     while True:
         cv2.imshow("AI", attitude_indicator_instrument(pitch, roll))
         cv2.waitKey(1)
-        pitch += 0.2 if pitch_inc else -0.2
+        pitch += 0.5 if pitch_inc else -0.5
         if pitch < -45.0: pitch_inc = True
         elif pitch > 45.0: pitch_inc = False
         roll += 0.2 if roll_inc else -0.2
