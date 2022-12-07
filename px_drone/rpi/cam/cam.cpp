a38725c589cfc99b785a099e7aed863adcd23922
nlao <lao.nathan@yahoo.com>
2022-12-06 22:38:14 -0600

    setup cam development

--- px_drone/rpi/cam/cam.cpp
+++ px_drone/rpi/cam/cam.cpp
@@ -3,20 +3,18 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <stdio.h>
-using namespace cv;
-using namespace std;
 
 int main(int, char **)
 {
-    Mat input;
-    Mat output;
+    cv::Mat input;
+    cv::Mat output;
 
-    vector<int> COMPRESSION_PARAMS;
-    COMPRESSION_PARAMS.push_back(IMWRITE_PNG_COMPRESSION);
+    std::vector<int> COMPRESSION_PARAMS;
+    COMPRESSION_PARAMS.push_back(cv::IMWRITE_PNG_COMPRESSION);
     COMPRESSION_PARAMS.push_back(0);
 
     //--- INITIALIZE VIDEOCAPTURE
-    VideoCapture cap;
+    cv::VideoCapture cap;
     // open the default camera using default API
     // cap.open(0);
     // OR advance usage: select any API backend
@@ -27,21 +25,25 @@ int main(int, char **)
     // check if we succeeded
     if (!cap.isOpened())
     {
-        cerr << "ERROR! Unable to open camera\n";
+        std::cerr << "ERROR! Unable to open camera\n";
         return -1;
     }
 
-    cap.read(input);
-    if (input.empty())
+    int idx = 0;
+    while(1)
     {
-        std::cerr << "ERROR!" << std::endl;
-    }
+    cap.read(input);
+    //if (input.empty())
+    //{
+    //    std::cerr << "ERROR!" << std::endl;
+    //}
 
     // Raw image is upside down
-    cv::rotate(input, output, ROTATE_180);
-
+    //cv::rotate(input, output, cv::ROTATE_180);
+    std::cout << idx++ << std::endl;
+    }
     // Temporary - testing
-    cv::imwrite("camtest.png", output, COMPRESSION_PARAMS);
+    //cv::imwrite("camtest.png", output, COMPRESSION_PARAMS);
 
     return 0;
 }
