4152f47321d54187efe7f8a32aaab3b71e5e98e7
Nate <lao.nathan@yahoo.com>
2022-12-11 11:42:16 -0600

    update deployement

--- px_drone/rpi/deploy.sh
+++ px_drone/rpi/deploy.sh
@@ -1,14 +1,22 @@
+# CLEAN SLATE
+rm -rvf ~/flypi.d
+mkdir ~/flypi.d
+
 # CAMERA DAEMON
 cd cam
-
+make clean
+make all
+mv -v flypi-cam.d ~/flypi.d/
 cd ..
 
+# GPS DAEMON
+cd gps
+
+cd ..
 
 # IMU DAEMON
 cd imu
 make clean
 make all
-rm -rvf ~/flypi.d
-mkdir ~/flypi.d
 mv -v flypi-imu.d ~/flypi.d/
 cd ..
