303cb616fb48cd3f22d12891519b0e96e0095172
Nate <lao.nathan@yahoo.com>
2022-12-11 14:25:20 -0600

    viewport inop screen - hysteresis

--- px_drone/host/viewport/viewport.py
+++ px_drone/host/viewport/viewport.py
@@ -9,17 +9,24 @@ import viewport_lib as lib
 
 sg.theme('Dark Blue')
 
+HYSTERESIS = 5
 FONT_SIZE = ('Any 30')
 LAYOUT = [
     [sg.Text('FlyPi ViewPort'), sg.Push(), sg.Text('             Author: Nate Lao (lao.nathan@yahoo.com)')],
     [sg.Image(filename='',key='--viewport--')]
     ]
 
+# RED X IF NO TRANSMISSION RECEIVED
+INOP_SCREEN = np.zeros((480,640,3), np.uint8)
+cv2.line(INOP_SCREEN, (0,0),  (640,480),(0,0,255),2)
+cv2.line(INOP_SCREEN, (0,480),  (640,0),(0,0,255),2)
+
 if __name__ == "__main__":
     window = sg.Window('FlyPi ViewPort', LAYOUT, finalize=True)
     # Setup Receiver
     receiver = lib.ETHERNET_ADAPTER_CAM()
 
+    persistance = 0
     while True:
         # Check to see if the cancel button was clicked and exit loop if clicked
         event, values = window.read(timeout=0)
@@ -28,6 +35,14 @@ if __name__ == "__main__":
 
         # Poll Rx
         payload = receiver.recv()
+
+        # Apply Hysteresis on Input
+        if payload != None:
+            cache = payload
+            persistance = HYSTERESIS
+        elif persistance > 0:
+            payload = cache
+            persistance -= 1
 
         # Show Live Footage
         if payload != None:
@@ -38,7 +53,8 @@ if __name__ == "__main__":
             imgbytes = cv2.imencode('.png', img)[1].tobytes() # this is faster, shorter and needs less includes
             window['--viewport--'].update(data=imgbytes)
         else:
-           pass
+            imgbytes = cv2.imencode('.png', INOP_SCREEN)[1].tobytes() # this is faster, shorter and needs less includes
+            window['--viewport--'].update(data=imgbytes)
             
     # Loop Terminated - Kill Window
     window.close()
